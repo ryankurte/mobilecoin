de2dac617bbba691dfdab4d3f6c3afd4d98a2809
ryan kurte <ryankurte@gmail.com>
2022-09-01 18:55:45 +1200

    generics to attempt to get heap under control

--- core/src/mlsag.rs
+++ core/src/mlsag.rs
@@ -20,7 +20,7 @@ use mc_crypto_ring_signature::{CurveScalar, KeyImage};
 use crate::protos;
 
 /// Maximum number of txouts in ring signature
-const MAX_TXOUTS: usize = 20;
+const MAX_TXOUTS: usize = 11;
 
 /// Maximum number of challenges in ring signature
 const MAX_RESPONSES: usize = MAX_TXOUTS * 2;
@@ -31,28 +31,28 @@ const MAX_RESPONSES: usize = MAX_TXOUTS * 2;
 /// Mirrors [`mc_crypto_ring_signature::RingMLSAG`] while using internal allocation,
 /// provides proto compatibility via [`crate::Rpc`] interface where `protos` feature is enabled.
 #[derive(Clone, PartialEq, Eq)]
-pub struct RingMLSAG {
+pub struct RingMLSAG<const RING_SIZE: usize = MAX_TXOUTS, const RESP_SIZE: usize = MAX_RESPONSES> {
     /// The initial challenge `c[0]`.
     pub c_zero: CurveScalar,
 
     /// Responses `r_{0,0}, r_{0,1}, ... , r_{ring_size-1,0},
     /// r_{ring_size-1,1}`.
-    pub responses: heapless::Vec<CurveScalar, MAX_RESPONSES>,
+    pub responses: heapless::Vec<CurveScalar, RESP_SIZE>,
 
     /// Key image "spent" by this signature.
     pub key_image: KeyImage,
 }
 
-impl RingMLSAG {
+impl <const RING_SIZE: usize, const RESP_SIZE: usize> RingMLSAG<RING_SIZE, RESP_SIZE> {
     /// Generate a ring signature with the provided options
     pub fn sign<'a>(opts: &MlsagSign<'a>, rng: &mut dyn CryptoRngCore) -> Result<Self, Error> {
         let ring_size = opts.ring.len();
 
         // Setup buffers for recomputed_c and decompressed rings
         let (mut challenges, mut responses, mut decompressed_ring) = (
-            heapless::Vec::<_, MAX_TXOUTS>::new(),
-            heapless::Vec::<_, MAX_RESPONSES>::new(),
-            heapless::Vec::<_, MAX_TXOUTS>::new(),
+            heapless::Vec::<_, RING_SIZE>::new(),
+            heapless::Vec::<_, RESP_SIZE>::new(),
+            heapless::Vec::<_, RING_SIZE>::new(),
         );
 
         challenges.resize(ring_size, Scalar::zero()).unwrap();
